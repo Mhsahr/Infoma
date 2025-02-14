48483e93a290ddd335ebbf6cbeb4a5e76a820f84
Mahsa <m.hamedd1380@gmail.com>
2025-02-14 14:56:16 +0330

    Initial commit

---
+++ Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Infoma.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Username {  get; set; }
+        public string Firstname {  get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }    
+    }
+}
