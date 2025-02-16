456c712961e6f05075e9beddba224f38cae565f6
Mahsa <m.hamedd1380@gmail.com>
2025-02-16 20:45:08 +0330

    Sign up view part one

--- Controllers/LogInController.cs
+++ Controllers/LogInController.cs
@@ -3,8 +3,22 @@
 namespace Infoma.Controllers
 {
     public class LogInController : Controller
+    { 
+        private readonly ILogger<LogInController> _logger;
+
+
+    public LogInController(ILogger<LogInController> logger)
     {
-        public IActionResult Index()
+        _logger = logger;
+
+    }
+    
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
         {
             return View();
         }
